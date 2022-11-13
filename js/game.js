6ba066e9ce86966a8f4ee8dbec25fc72e0eedace
patridifusa <p.rufino.a@gmail.com>
2022-11-13 22:32:47 +0000

    Add functions in game JS file and test file

--- js/game.js
+++ js/game.js
@@ -1,1 +1,77 @@
-export const game = () => {};
+let grid = [];
+let row1 = [];
+let row2 = [];
+let row3 = [];
+let cell1;
+let cell2;
+let cell3;
+let aliveCell = 'o';
+let deadCell = 'x';
+let countCells = 0;
+
+// Obtener un valor random entre dos valores enteros
+export const randomValue = () => {
+    let value = Math.floor(Math.random() * 2);
+    return value;
+};
+
+// Asignar valor inicial a cada célula
+export const valueCell = () => {
+    let valueCell = randomValue();
+    if (valueCell === 1) {
+        valueCell = aliveCell;
+    } else {
+        valueCell = deadCell;
+    }
+    return valueCell;
+};
+
+// Pintar grid
+export const drawGrid = () => {
+    grid.push(row1, row2, row3);
+    grid.forEach((element) => {
+        element.push(valueCell(), valueCell(), valueCell());
+    });
+    console.table(grid);
+    return grid;
+};
+
+// Contar células vivas
+const countCellsAlive = () => {
+    // Recorre todos los elementos del grid
+    grid.forEach((rowItem) => {
+        rowItem.forEach((cellItem) => {
+            if (cellItem === aliveCell) {
+                countCells++;
+            }
+        });
+    });
+    console.log(countCells);
+};
+
+//
+
+// Ciclo de vida de las células
+const lifeCycleCell = (countCells) => {
+    // Estado de la célula
+    let isAlive = true;
+    if (!countCells >= 2 || !countCells <= 3) {
+        // La célula muere
+        isAlive = false;
+        row2[1] = deadCell;
+        return console.log(row2);
+    } else if (countCells === 3) {
+        // La célula vive
+        isAlive = true;
+        row2[1] = aliveCell;
+        return console.log(row2);
+    }
+};
+
+export const gameOfLife = () => {
+    drawGrid();
+    countCellsAlive();
+    lifeCycleCell();
+};
+
+gameOfLife();
--- js/game.test.js
+++ js/game.test.js
@@ -1,1 +1,30 @@
-import { gameOfLife } from './game.js';
+import { randomValue, valueCell, drawGrid } from './game.js';
+
+// RandomValue Function
+describe('Get a integer random number between 0 and 1', () => {
+    const result = randomValue();
+    const expected = 0;
+    const expected1 = 1;
+    test(`The random value function return ${result} and is expected ${expected}`, () => {
+        expect(result).toBe(expected || expected1);
+    });
+});
+
+// ValueCell Function
+describe('Assign an status cell to random value', () => {
+    const result = valueCell();
+    const expected = 'x';
+    const expected1 = 'o';
+    test(`The value cell function return ${result} and is expected ${expected}`, () => {
+        expect(result).toBe(expected || expected1);
+    });
+});
+
+// DragGrid Function
+describe('Drag a grid with cells', () => {
+    const result = drawGrid();
+    const expected = [[], [], []];
+    test(`The value cell function return ${result} and is expected ${expected}`, () => {
+        expect(result).toEqual(expected);
+    });
+});
