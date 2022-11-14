3132f77e62c76f71549904abfb52ea035c972d85
patridifusa <p.rufino.a@gmail.com>
2022-11-14 23:38:39 +0000

    Game of life function working

--- js/game.js
+++ js/game.js
@@ -1,88 +1,59 @@
-//let grid = [];
-let row1 = [];
-let row2 = [];
-let row3 = [];
-// let aliveCell = 'o';
-// let deadCell = 'x';
-// let countCells = 0;
+let grid = [];
+const numRows = 9;
+const numCols = 9;
+let aliveCell = 'o';
+let deadCell = ' ';
+let countCells = 0;
 
 // Obtener un valor random entre dos valores enteros
-// export const randomValue = () => {
-//     let value = Math.floor(Math.random() * 2);
-//     return value;
-// };
+const randomValue = () => {
+    let value = Math.floor(Math.random() * 2);
+    return value;
+};
 
 // // Asignar valor inicial a cada célula
-// export const valueCell = () => {
-//     let valueCell = randomValue();
-//     if (valueCell === 1) {
-//         valueCell = aliveCell;
-//     } else {
-//         valueCell = deadCell;
-//     }
-//     return valueCell;
-// };
+const valueCell = () => {
+    let valueCell = randomValue();
+    if (valueCell === 1) {
+        valueCell = aliveCell;
+    } else {
+        valueCell = deadCell;
+    }
+    return valueCell;
+};
 
 // // Pintar grid
-// export const drawGrid = () => {
-//     grid.push(row1, row2, row3);
-//     grid.forEach((element) => {
-//         element.push(valueCell(), valueCell(), valueCell());
-//     });
-//     return grid;
-// };
-
-// Contar células vivas
-// const countCellsAlive = () => {
-//     // Recorre todos los elementos del grid
-//     grid.forEach((rowItem) => {
-//         rowItem.forEach((cellItem) => {
-//             if (cellItem === aliveCell) {
-//                 countCells++;
-//             }
-//         });
-//     });
-//     console.log(countCells);
-// };
-
-// comentar condicional primero
-// if (rowItem[i - 1] === aliveCell) {
-//     countCells++;
-// }
-// if (rowItem[i + 1] === aliveCell) {
-//     countCells++;
-// }
-
-// Buscar células contiguas a cada una
-const grid = [
-    ['x', 'o', 'x'],
-    ['x', 'o', 'x'],
-    ['o', 'o', 'x'],
-];
-let aliveCell = 'o';
-let deadCell = 'x';
-let countCells = 0;
+const drawGrid = () => {
+    for (let ro = 0; ro < numRows; ro++) {
+        grid.push([]);
+    }
+    grid.forEach((element) => {
+        for (let co = 0; co < numCols; co++) {
+            element.push(valueCell());
+        }
+    });
+    return grid;
+};
 
 // Ciclo de vida de las células
 const lifeCycleCell = (countCells, index, indexCell) => {
-    debugger;
     let currentCell = grid[index][indexCell];
     // Estado de la célula
-    let isAlive = true;
     if (currentCell === deadCell && countCells === 3) {
         // La célula vive
-        isAlive = true;
         grid[index][indexCell] = aliveCell;
     }
     if (
-        (currentCell === aliveCell && countCells <= 2) ||
-        (currentCell === aliveCell && countCells >= 3)
+        (currentCell === aliveCell && countCells < 2) ||
+        (currentCell === aliveCell && countCells > 3)
     ) {
-        isAlive = false;
+        // La célula muere
         grid[index][indexCell] = deadCell;
     }
-    console.table(grid);
+    return grid;
 };
+
+// Buscar células contiguas a cada una
 const siblingCells = () => {
     let currentCell;
     let beforeCell;
@@ -96,12 +67,10 @@ const siblingCells = () => {
     let afterRowOnCell;
     let afterRowAfterCell;
 
-    debugger;
     console.table(grid);
     for (let i = 0; i < grid.length; i++) {
         for (let z = 0; z < grid[i].length; z++) {
             currentCell = grid[i][z];
-            debugger;
             if (i === 0) {
                 beforeCell = grid[i][z - 1];
                 if (beforeCell === aliveCell) {
@@ -186,14 +155,11 @@ const siblingCells = () => {
     }
     return grid;
 };
-siblingCells();
 
 export const gameOfLife = () => {
-    //drawGrid();
-    //Esta función está pendiente de saber si se usa o no
-    //countCellsAlive();
-    siblingCells();
-    //lifeCycleCell();
+    drawGrid();
+    //siblingCells();
+    setInterval(siblingCells, 100);
 };
 
 gameOfLife();
